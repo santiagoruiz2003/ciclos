7e272c34241782e8a29068be7b0cdc7465dcdb7a
santiagoruiz2003 <santiagoruiz5546@gmail.com>
2022-06-25 08:42:36 -0500

    cambios

--- src/ciclos.java
+++ src/ciclos.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class ciclos {
     public static void main(String[] args) {
+        for (int i = 0; i < 3; i++){
+            JOptionPane.showInputDialog(null,"hola mundo");
+        }
 
     }
 }
---
+++ src/numeros.java
@@ -0,0 +1,9 @@
+public class numeros {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            System.out.println(" numero: "+i);
+
+        }
+
+    }
+}
